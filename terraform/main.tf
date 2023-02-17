6d9851675c7e6659a00ffd506a7adb877287ce4f
Kulbhushan <kulbhushan.mayer@gmail.com>
2023-02-17 12:49:15 +0530

    added code block for database creation under cosmos db account

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,6 +8,18 @@ variable "location" {
 variable "name" {
   default = "thinknyx"
 }
+variable "throughput" {
+  type        = number
+  description = "Cosmos db database throughput"
+  validation {
+    condition     = var.throughput >= 400 && var.throughput <= 1000000
+    error_message = "Cosmos db manual throughput should be equal to or greater than 400 and less than or equal to 1000000."
+  }
+  validation {
+    condition     = var.throughput % 100 == 0
+    error_message = "Cosmos db throughput should be in increments of 100."
+  }
+}
 
 resource "azurerm_resource_group" "thinknyx" {
   location = var.location
@@ -33,4 +45,11 @@ resource "azurerm_cosmosdb_account" "GlobalDocumentDB" {
     max_interval_in_seconds = 300
     max_staleness_prefix    = 100000
   }
+}
+
+resource "azurerm_cosmosdb_sql_database" "thinknyx" {
+  name                = var.name
+  resource_group_name = azurerm_resource_group.thinknyx.name
+  account_name        = azurerm_cosmosdb_account.example.name
+  throughput          = var.throughput
 }
